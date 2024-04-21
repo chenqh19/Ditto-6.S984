8e359aae183b78978dea2b2cea1c90b5a1c64fab
chenqh23 <chenqh23@localhost>
2024-04-21 16:59:41 -0600

    update

--- synthetic_social_network/scripts/auto.py
+++ synthetic_social_network/scripts/auto.py
@@ -7,18 +7,18 @@ config = "/users/chenqh23/Ditto-6.S984/synthetic_social_network/config/"
 
 def main(argv):
   path_id = argv[0]
-  with open(config + 'services.json') as load_services_file:
-    services_json = json.load(load_services_file)
-    for service in services_json:
-      if os.path.exists("../src/" + service):
-        os.system('rm -rf ../src/' + service)
+  # with open(config + 'services.json') as load_services_file:
+  #   services_json = json.load(load_services_file)
+  #   for service in services_json:
+  #     if os.path.exists("../src/" + service):
+  #       os.system('rm -rf ../src/' + service)
   # os.system('docker rmi ' + image_name)
   os.system('rm -rf ../gen-cpp')
   os.system('rm -rf ../gen-lua')
 
   os.system('python2 thrift_gen.py ' + config)
   os.system('python2 lua_rewrite.py')
-  os.system('python2 src_files_gen.py ' + path_id + ' ' + config)
+  # os.system('python2 src_files_gen.py ' + path_id + ' ' + config)
   os.system('python2 docker-compose_gen.py ' + path_id + ' ' + image_name + ' ' + config)
 
 if __name__ == "__main__":
