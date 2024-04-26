2c3cf06165bf3d168347a550cabec9d04da072c9
chenqh23 <chenqh23@localhost>
2024-04-26 08:57:10 -0600

    update

--- synthetic_social_network/scripts/batched_test.py
+++ synthetic_social_network/scripts/batched_test.py
@@ -18,7 +18,7 @@ def run_command(command, output_file):
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         for line in process.stdout:
             decoded_line = line.decode().strip()
-            if "0.900000" in decoded_line:
+            if "0.950000" in decoded_line:
                 f.write(decoded_line + "\n")
         process.stdout.close()
         process.stderr.close()
@@ -34,13 +34,14 @@ def run_cmd(gen_work_cmd, output_file):
     # process2.join()
 
 def refresh():
-    refresh_cmd = "sudo docker service update --force synthetic_social_network-jaeger-1"
+    # refresh_cmd = "sudo docker service update --force synthetic_social_network-jaeger-1"
+    refresh_cmd = "sudo docker restart synthetic_social_network-jaeger-1"
     subprocess.run(refresh_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
 output_file = "../output/example.txt"
 base_cmd = "../../wrk2/wrk -D exp -t 20 -c 100 -d 60 -L http://localhost:8010/api/service_0/rpc_0 -R "
 
-rpss = set_range(400, 800, 40)
+rpss = set_range(600, 1000, 40)
 
 for rps in rpss:
     with open(output_file, "a") as f:
