72de357744377b06eac9da293d42af2727184865
chenqh23 <chenqh23@localhost>
2024-04-25 22:09:55 -0600

    update

---
+++ synthetic_social_network/scripts/extract.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def find_second_smallest(numbers):
+    unique_numbers = sorted(set(numbers))
+    if len(unique_numbers) > 1:
+        return unique_numbers[1]
+    else:
+        return None
+
+def find_mid_avg(numbers):
+    unique_numbers = sorted(set(numbers))
+    return unique_numbers[1]
+    if len(unique_numbers) > 1:
+        return round((unique_numbers[1]+unique_numbers[2]+unique_numbers[3])/3, 1)
+    else:
+        return None
+
+def main():
+    count = 0
+    results = []
+    with open('../output/example.txt', 'r') as file:
+        lines = file.readlines()
+        for i in range(len(lines)):
+            if "new_config" in lines[i]:
+                if i + 3 < len(lines):
+                    numbers = [float(line.split()[0]) for line in lines[i+1:i+4]]
+                    rep_num = find_mid_avg(numbers)
+                    if rep_num is not None:
+                        results.append(rep_num)
+                    else:
+                        print(f"Not enough numbers at {count} to find the latency.")
+                count += 1
+    print(results)
+
+    print(f"Total count: {count}")
+
+if __name__ == "__main__":
+    main()
